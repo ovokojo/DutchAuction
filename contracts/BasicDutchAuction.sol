b995ec29dccfe5114d658e4f7e70e45232dc6c4e
ovokojo <kojoaddaquay@gmail.com>
2023-02-01 13:37:35 -0500

    make contract ownable, added tests for initialization parameters

--- contracts/BasicDutchAuction.sol
+++ contracts/BasicDutchAuction.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BasicDutchAuction {
+contract BasicDutchAuction is Ownable {
     /*
      Initialization parameters
     */
     // the minimum amount of wei that the seller is willing to accept for the item
-    uint256 private reservePrice = 100;
+    uint256 private reservePrice = 0.1 ether;
     // the number of blockchain blocks that the auction is open for
     uint256 private numBlocksAuctionOpen = 10;
     // the amount of wei that the auction price should decrease by during each subsequent block.
-    uint256 private offerPriceDecrement = 1;
+    uint256 private offerPriceDecrement = 0.001 ether;
     // The current price of the item
     uint256 private auctionPrice;
     // Whether the auction is open or closed
-    bool private auctionOpen = true;
+    bool private auctionOpen;
     /*
      Stakeholders
     */
-    // the owner of the contract
-    address private owner;
     // the seller of the item
     address private seller;
     // the winning buyer of the item
@@ -30,22 +29,37 @@ contract BasicDutchAuction {
     mapping(address => uint256) public bids;
 
     constructor() {
-        owner = msg.sender;
+        // Open the auction
+        auctionOpen = true;
         // Set the initial price of the item
         auctionPrice =
             reservePrice +
-            numBlocksAuctionOpen *
-            offerPriceDecrement;
-    }
-
-    // View owner address
-    function getOwner() public view returns (address) {
-        return owner;
+            (numBlocksAuctionOpen * offerPriceDecrement);
     }
 
     // View current price
     function getReservePrice() public view returns (uint256) {
         return reservePrice;
+    }
+
+    // View current price
+    function getNumBlocksAuctionOpen() public view returns (uint256) {
+        return numBlocksAuctionOpen;
+    }
+
+    // View number of blockchain blocks that the auction is open for
+    function getOfferPriceDecrement() public view returns (uint256) {
+        return offerPriceDecrement;
+    }
+
+    // View current price
+    function getAuctionPrice() public view returns (uint256) {
+        return auctionPrice;
+    }
+
+    // View auction status
+    function getAuctionStatus() public view returns (bool) {
+        return auctionOpen;
     }
 
     // function to bid on the item
--- test/BasicDutchAuction.ts
+++ test/BasicDutchAuction.ts
@@ -1,4 +1,4 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
@@ -8,7 +8,7 @@ describe("BasicDutchAuction", function () {
   // and reset Hardhat Network to that snapshot in every test.
   async function deployBasicDutchAuctionFixture() {
     // Contracts are deployed using the first signer/account by default
-    const [owner] = await ethers.getSigners();
+    const [owner, firstBidder, secondBidder] = await ethers.getSigners();
     const basicDutchAuctionFactory = await ethers.getContractFactory("BasicDutchAuction");
     const basicDutchAuction = await basicDutchAuctionFactory.deploy();
 
@@ -16,13 +16,46 @@ describe("BasicDutchAuction", function () {
   }
 
   describe("Deployment", function () {
-    it("Contract should be owned by deployer", async function () {
+    it("should have the right owner", async function () {
       const { basicDutchAuction, owner } = await loadFixture(deployBasicDutchAuctionFixture);
-      expect(await basicDutchAuction.getOwner()).to.equal(owner.address);
+      expect(await basicDutchAuction.owner()).to.equal(owner.address);
     });
-    it("Reserve Price price should be equal to 100", async function () {
-      const { basicDutchAuction, owner } = await loadFixture(deployBasicDutchAuctionFixture);
-      expect(await basicDutchAuction.getReservePrice()).to.equal(100);
+
+    it("should have the right reserve price", async function () {
+      const { basicDutchAuction } = await loadFixture(deployBasicDutchAuctionFixture);
+      const amountInWei = ethers.utils.parseUnits("0.1", "ether")
+      expect(await basicDutchAuction.getReservePrice()).to.equal(amountInWei);
+    });
+    it("should have the right number of blockchain blocks that the auction is open for", async function () {
+      const { basicDutchAuction } = await loadFixture(deployBasicDutchAuctionFixture);
+      expect(await basicDutchAuction.getNumBlocksAuctionOpen()).to.equal(10);
+    });
+
+    it("should have the right price decrement after each block", async function () {
+      const { basicDutchAuction } = await loadFixture(deployBasicDutchAuctionFixture);
+      const amountInWei = ethers.utils.parseUnits("0.001", "ether")
+      expect(await basicDutchAuction.getOfferPriceDecrement()).to.equal(amountInWei);
+    });
+
+    it("should have the right price decrement after each block", async function () {
+      const { basicDutchAuction } = await loadFixture(deployBasicDutchAuctionFixture);
+      const amountInWei = ethers.utils.parseUnits("0.001", "ether")
+      expect(await basicDutchAuction.getOfferPriceDecrement()).to.equal(amountInWei);
+    });
+
+    it("should have the right initial price at auction open", async function () {
+      const { basicDutchAuction } = await loadFixture(deployBasicDutchAuctionFixture);
+      const reservePrice = await basicDutchAuction.getReservePrice();
+      const numBlocksAuctionOpen = await basicDutchAuction.getNumBlocksAuctionOpen();
+      const offerPriceDecrement = await basicDutchAuction.getOfferPriceDecrement();
+      expect(reservePrice.add(numBlocksAuctionOpen.mul(offerPriceDecrement))).to.equal(110000000000000000n);
+      expect(await basicDutchAuction.getAuctionPrice()).to.equal(110000000000000000n);
+      expect(reservePrice.add(numBlocksAuctionOpen.mul(offerPriceDecrement))).to.equal(await basicDutchAuction.getAuctionPrice());
+    });
+
+    it("should have the auction open", async function () {
+      const { basicDutchAuction } = await loadFixture(deployBasicDutchAuctionFixture);
+      expect(await basicDutchAuction.getAuctionStatus()).to.equal(true);
     });
   });
 });
