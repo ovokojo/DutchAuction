451ca18de9cdac5fa98fe585c0ace3ff3ece304e
ovokojo <kojoaddaquay@gmail.com>
2023-02-18 00:53:44 +0000

    added ERC20 bids & unit tests

--- V3/contracts/NFTDuctionAuction_ERC20Bids.sol
+++ V3/contracts/NFTDuctionAuction_ERC20Bids.sol
@@ -5,8 +5,9 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract NFTDutchAuction is Ownable, ReentrancyGuard {
+contract NFTDuctionAuction_ERC20Bids is Ownable, ReentrancyGuard {
     /*
      Initialization parameters
     */
@@ -14,6 +15,8 @@ contract NFTDutchAuction is Ownable, ReentrancyGuard {
     IERC721 public nft;
     // Token id of NFT
     uint256 private nftTokenId;
+    // ERC20 bid token
+    IERC20 public acceptedToken;
     // the minimum amount of wei that the seller is willing to accept for the item
     uint256 private reservePrice;
     // Initial block number
@@ -37,6 +40,7 @@ contract NFTDutchAuction is Ownable, ReentrancyGuard {
     mapping(address => uint256) public _bids;
 
     constructor(
+        address erc20TokenAddress,
         address erc721TokenAddress,
         uint256 _nftTokenId,
         uint256 _reservePrice,
@@ -44,8 +48,11 @@ contract NFTDutchAuction is Ownable, ReentrancyGuard {
         uint256 _offerPriceDecrement
     ) {
         // Set auction parameters
+        // Set NFT item
         nftTokenId = _nftTokenId;
         nft = IERC721(erc721TokenAddress);
+        // Set ERC-20 token
+        acceptedToken = IERC20(erc20TokenAddress);
         reservePrice = _reservePrice;
         numBlocksAuctionOpen = _numBlocksAuctionOpen;
         offerPriceDecrement = _offerPriceDecrement;
@@ -84,6 +91,11 @@ contract NFTDutchAuction is Ownable, ReentrancyGuard {
         return _initialPrice;
     }
 
+    // Get buyer address
+    function getBuyer() public view returns (address) {
+        return _buyer;
+    }
+
     // View current price
     function getCurrentPrice() public view returns (uint256) {
         return
@@ -112,48 +124,51 @@ contract NFTDutchAuction is Ownable, ReentrancyGuard {
         return block.number;
     }
 
-    // function to bid on the item
-    function bid() public payable nonReentrant {
-        // check that the auction is still open
+    function bid(uint256 bidAmount) public nonReentrant {
+        // Check that the auction is still open
         require(isAuctionOpen() == true, "Auction is closed!");
-        // check that the bid is greater than the reserve price
-        require(msg.value >= reservePrice, "Bid is below reserve price!");
-        // Add current bidder to list of bidders
+        // Check that the bid is greater than the reserve price
+        require(bidAmount >= reservePrice, "Bid is below reserve price!");
+        // Transfer tokens from bidder to the contract
+        acceptedToken.transferFrom(msg.sender, address(this), bidAmount);
+        // Add current bidder to the list of bidders
         _bidders.push(payable(msg.sender));
-        // if bid is greater than or equal to current price, declare winner, refund other bidders & close auction
-        if (msg.value >= getCurrentPrice()) {
-            // Declare winner & close auction
+
+        // If the bid is greater than or equal to the current price, declare the winner, refund other bidders, and close the auction
+        if (bidAmount >= getCurrentPrice()) {
+            // Declare the winner & close the auction
             _buyer = msg.sender;
-            // Close auction
             _auctionOpen = false;
+
             // Send money to the seller, assuming it is the contract owner
-            (bool purchaseSuccess, ) = owner().call{value: msg.value}("");
-            // Approve buyer
+            acceptedToken.transfer(owner(), bidAmount);
+
+            // Approve the buyer
             nft.approve(msg.sender, nftTokenId);
-            // Transfer nft to buyer
+
+            // Transfer the NFT to the buyer
             nft.transferFrom(nft.ownerOf(nftTokenId), msg.sender, nftTokenId);
-            if (!purchaseSuccess) {
-                revert();
-            }
+
+            // Record the winning bid
+            _bids[msg.sender] = bidAmount;
+
             // Refund all other bidders
             for (uint i = 0; i < _bidders.length; i++) {
                 address payable bidder = _bidders[i];
-                // require contract has enough money to refund
-                require(
-                    address(this).balance >= _bids[bidder],
-                    "Not enough funds to refund bidder!"
-                );
-                (bool refundSuccess, ) = bidder.call{value: _bids[bidder]}("");
-                if (!refundSuccess) {
-                    revert();
+
+                if (bidder != msg.sender) {
+                    uint256 bidToRefund = _bids[bidder];
+                    require(
+                        acceptedToken.balanceOf(address(this)) >= bidToRefund,
+                        "Not enough tokens to refund bidder!"
+                    );
+                    acceptedToken.transfer(bidder, bidToRefund);
+                    _bids[bidder] = 0;
                 }
-                _bids[bidder] = 0;
             }
-            // Record winning bid
-            _bids[msg.sender] = msg.value;
         } else {
-            // Otherwise, record bid amount by bidder
-            _bids[msg.sender] += msg.value;
+            // Otherwise, accept and record the bid amount
+            _bids[msg.sender] += bidAmount;
         }
     }
 }
--- V3/test/NFTDuctionAuction_ERC20Bids.ts
+++ V3/test/NFTDuctionAuction_ERC20Bids.ts
@@ -16,12 +16,24 @@ describe("NFTDutchAuction", async function () {
     const dutchNFT = await DutchNFTFactory.deploy();
     // Mint NFT
     await dutchNFT.mint(owner.address);
+    // Deploy the DutchCoin contract
+    const DutchCoinFactory = await ethers.getContractFactory("DutchCoin");
+    const dutchCoin = await DutchCoinFactory.connect(owner).deploy(ethers.BigNumber.from(10).pow(18).mul(3000000));
+
+    // Distribute some DutchCoins to the bidders
+    const dutchCoinDecimals = await dutchCoin.decimals();
+    await dutchCoin.connect(owner).transfer(firstBidder.address, ethers.BigNumber.from(10).pow(dutchCoinDecimals).mul(1000000));
+    await dutchCoin.connect(owner).transfer(secondBidder.address, ethers.BigNumber.from(10).pow(dutchCoinDecimals).mul(1000000));
+
     // Deploy the auction contract
-    const basicDutchAuctionFactory = await ethers.getContractFactory("NFTDutchAuction");
-    const basicDutchAuction = await basicDutchAuctionFactory.deploy(dutchNFT.address, 1, 1000000000000000n, 10n, 100000000n);
+    const basicDutchAuctionFactory = await ethers.getContractFactory("NFTDuctionAuction_ERC20Bids");
+    const basicDutchAuction = await basicDutchAuctionFactory.deploy(dutchCoin.address, dutchNFT.address, 1, 1000000000000000n, 100n, 100000000n);
     // Set the auction contract as an approved operator for the NFT
     await dutchNFT.setApprovalForAll(basicDutchAuction.address, true);
-    return { basicDutchAuction, provider, owner, dutchNFT, firstBidder, secondBidder };
+    // Approve the auction contract to spend tokens on behalf of the bidders
+    await dutchCoin.connect(firstBidder).approve(basicDutchAuction.address, ethers.BigNumber.from(10).pow(dutchCoinDecimals).mul(1000000));
+    await dutchCoin.connect(secondBidder).approve(basicDutchAuction.address, ethers.BigNumber.from(10).pow(dutchCoinDecimals).mul(1000000));
+    return { basicDutchAuction, provider, owner, dutchNFT, firstBidder, secondBidder, dutchCoin };
   }
   /// Simulates increasing the number of blocks mined
   async function mineBlocks(provider: JsonRpcProvider, blockCount: number) {
@@ -56,15 +68,13 @@ describe("NFTDutchAuction", async function () {
     });
   });
 
-  describe("Auction process", function () {
+  describe("Auction process with ERC-20", function () {
     it("should be open if within the number of required blocks", async function () {
-      const { basicDutchAuction, provider } = await loadFixture(deployBasicDutchAuctionFixture);
+      const { basicDutchAuction } = await loadFixture(deployBasicDutchAuctionFixture);
       const numBlocksAuctionOpen = await basicDutchAuction.getNumBlocksAuctionOpen();
-      const initialBlock = await basicDutchAuction.getInitialBlock();
-      await mineBlocks(provider, numBlocksAuctionOpen.toNumber());
       const currentBlock = await basicDutchAuction.getCurrentBlock();
-      expect(initialBlock.add(numBlocksAuctionOpen)).lessThanOrEqual(currentBlock);
       const isAuctionOpen = await basicDutchAuction.isAuctionOpen();
+      expect(currentBlock).lessThanOrEqual(numBlocksAuctionOpen);
       expect(isAuctionOpen).equal(true);
     });
 
@@ -79,25 +89,23 @@ describe("NFTDutchAuction", async function () {
 
 
     it("should record a bid below current price", async function () {
-      const { basicDutchAuction, firstBidder } = await loadFixture(deployBasicDutchAuctionFixture);
+      const { basicDutchAuction, firstBidder, dutchCoin } = await loadFixture(deployBasicDutchAuctionFixture);
       const currentPrice = await basicDutchAuction.getCurrentPrice();
       const bidAmount = currentPrice.sub(1);
-      await basicDutchAuction.connect(firstBidder).bid({
-        value: bidAmount,
-      });
-      const bidAddress = await basicDutchAuction._bidders(0)
+      await dutchCoin.connect(firstBidder).approve(basicDutchAuction.address, bidAmount);
+      await basicDutchAuction.connect(firstBidder).bid(bidAmount);
+      const bidAddress = await basicDutchAuction._bidders(0);
       const bid = await basicDutchAuction._bids(bidAddress);
       expect(bidAddress).to.equal(firstBidder.address);
       expect(bid).to.equal(bidAmount);
     });
 
     it("should transfer the NFT to the winning bidder", async function () {
-      const { basicDutchAuction, firstBidder, dutchNFT } = await loadFixture(deployBasicDutchAuctionFixture);
+      const { basicDutchAuction, firstBidder, dutchNFT, dutchCoin } = await loadFixture(deployBasicDutchAuctionFixture);
       const currentPrice = await basicDutchAuction.getCurrentPrice();
-      const bidAmount = currentPrice.add(1n);
-      await basicDutchAuction.connect(firstBidder).bid({
-        value: bidAmount,
-      });
+      const bidAmount = currentPrice.add(1);
+      await dutchCoin.connect(firstBidder).approve(basicDutchAuction.address, bidAmount);
+      await basicDutchAuction.connect(firstBidder).bid(bidAmount);
       const owner = await dutchNFT.ownerOf(1);
       expect(owner).to.equal(firstBidder.address);
     });
@@ -106,9 +114,7 @@ describe("NFTDutchAuction", async function () {
       const { basicDutchAuction, firstBidder } = await loadFixture(deployBasicDutchAuctionFixture);
       const currentPrice = await basicDutchAuction.getCurrentPrice();
       const bidAmount = currentPrice.add(1n);
-      await basicDutchAuction.connect(firstBidder).bid({
-        value: bidAmount,
-      });
+      await basicDutchAuction.connect(firstBidder).bid(bidAmount);
       const auctionStatus = await basicDutchAuction.isAuctionOpen();
       expect(auctionStatus).to.equal(false);
     });
@@ -120,41 +126,36 @@ describe("NFTDutchAuction", async function () {
       const reservePrice = await basicDutchAuction.getReservePrice();
       const auctionStatus = await basicDutchAuction.isAuctionOpen();
       expect(auctionStatus).equal(false);
-      expect(basicDutchAuction.bid({ value: reservePrice.sub(1) })).to.be.reverted;
+      expect(basicDutchAuction.bid(reservePrice.sub(1))).to.be.reverted;
     });
 
     it("should reject a bid lower than the reserve price", async function () {
       const { basicDutchAuction } = await loadFixture(deployBasicDutchAuctionFixture);
       const reservePrice = await basicDutchAuction.getReservePrice();
-      expect(basicDutchAuction.bid({ value: reservePrice.sub(1) })).to.be.reverted;
+      expect(basicDutchAuction.bid(reservePrice.sub(1))).to.be.reverted;
     });
 
     it("should reject a second bid", async function () {
       const { basicDutchAuction, firstBidder, secondBidder } = await loadFixture(deployBasicDutchAuctionFixture);
       const currentPrice = await basicDutchAuction.getCurrentPrice();
-      expect(basicDutchAuction.connect(firstBidder).bid({
-        value: currentPrice.add(1),
-      })).to.be.not.reverted;
-      expect(basicDutchAuction.connect(secondBidder).bid({
-        value: currentPrice.add(1),
-      })).to.be.reverted;
+      expect(basicDutchAuction.connect(firstBidder).bid(currentPrice.add(1))).to.be.not.reverted;
+      expect(basicDutchAuction.connect(secondBidder).bid(currentPrice.add(1))).to.be.reverted;
     });
 
     it("should refund unsuccessful bidders", async function () {
-      const { basicDutchAuction, firstBidder, secondBidder, provider } = await loadFixture(deployBasicDutchAuctionFixture);
+      const { basicDutchAuction, firstBidder, secondBidder } = await loadFixture(deployBasicDutchAuctionFixture);
       const reservePrice = await basicDutchAuction.getReservePrice();
       const currentPrice = await basicDutchAuction.getCurrentPrice();
       const firstBidAmount = reservePrice.add(1);
       const secondBidAmount = currentPrice.add(1);
-      const initialBalance = await firstBidder.getBalance();
-      await basicDutchAuction.connect(firstBidder).bid({
-        value: firstBidAmount,
-      });
-      const currentBalance = await firstBidder.getBalance();
-      await basicDutchAuction.connect(secondBidder).bid({
-        value: secondBidAmount,
-      });
-      expect(initialBalance.sub(currentBalance)).to.greaterThanOrEqual(firstBidAmount);
+      await basicDutchAuction.connect(firstBidder).bid(firstBidAmount);
+      const balance1 = (await basicDutchAuction._bids(firstBidder.address)).toString();
+      expect(balance1).to.equal(firstBidAmount);
+      await basicDutchAuction.connect(secondBidder).bid(secondBidAmount);
+      const balance2 = (await basicDutchAuction._bids(secondBidder.address)).toString();
+      const updatedBalance1 = (await basicDutchAuction._bids(firstBidder.address)).toString();
+      expect(updatedBalance1).to.equal("0");
+      expect(balance2).to.equal(secondBidAmount);
     });
   });
 });
